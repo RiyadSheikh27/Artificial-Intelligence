5293a1263b4485e1db6fad18f53235232c76bfda
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2024-08-16 01:23:29 +0600

    Arithmetic Operation in Numpy

---
+++ Numpy/Arithmetic Operation/1D.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+var = np.array([1,2,3,4])
+print(var+3) #or print(np.add(var,3))
---
+++ Numpy/Arithmetic Operation/2D.py
@@ -0,0 +1,20 @@
+import numpy as np
+var1 = np.array([[1,2,3,4],[1,2,3,4]])
+var2 = np.array([[1,2,3,4],[1,2,3,4]])
+
+var = var1+var2
+print(var)
+
+print("Min: ",np.min(var),np.argmin(var))
+print()
+print("max: ",np.max(var),np.argmax(var))
+print()
+print("Max in x axis: ",np.max(var,axis=0))
+print()
+print("Sqrt: ",np.sqrt(var))
+print()
+print("Sin: ",np.sin(var))
+print()
+print("Cos: ",np.cos(var))
+print()
+print("Cumsum: ",np.cumsum(var1))
